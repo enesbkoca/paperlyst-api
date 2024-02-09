bd24f7bb4833c3d93b000e68d1d4f2fbae8aeb8c
Berkay_HP <enesbkoca@gmail.com>
2024-02-09 16:24:54 +0100

    Implement collection in chromadb

--- src/database.py
+++ src/database.py
@@ -4,10 +4,13 @@ import chromadb
 class Database:
 
     def __init__(self):
+        # change Client to PersistentClient when want to keep database fixed
         self.client = chromadb.Client()
+        self.collection = None
+        self.embedding_function = None
 
     def create_collections(self):
         self.collection = self.client.create_collection(
             name="abstract",
-            embedding_function=emb_fn
+            embedding_function=self.embedding_function
         )
