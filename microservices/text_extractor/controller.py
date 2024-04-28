9ae6c385bd68e02373bfa3995ade15348162b227
mtilerisoy <mtilerisoy@gmail.com>
2024-04-28 15:33:30 +0200

    implement text_extractor service

--- microservices/text_extractor/controller.py
+++ microservices/text_extractor/controller.py
@@ -1,13 +1,18 @@
-# from flask import Blueprint, jsonify
-#
-# microservice1_controller = Blueprint('microservice1', __name__)
-#
-# @microservice1_controller.route('/endpoint', methods=['POST'])
-# def endpoint_handler():
-#     data = request.json
-#
-#     # Call service method for microservice 1
-#     result = microservice1_service.process_data(data)
-#
-#     # Format and return response
-#     return jsonify(result)
+from flask import Blueprint, jsonify, request
+import requests
+from service import Microservice1Service
+
+microservice1_controller = Blueprint('microservice1', __name__)
+microservice1_service = Microservice1Service()
+@microservice1_controller.route('/endpoint', methods=['POST'])
+def endpoint_handler():
+    file_path = request.json['file_path']
+
+    # Call service method for microservice 1
+    result = microservice1_service.process_data(file_path)
+
+    # Send HTTP POST request with the extracted text
+    response = requests.post('http://localhost:9021', data=result)
+
+    # Format and return response
+    return jsonify(response.text)
